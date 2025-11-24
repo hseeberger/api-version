77ddf2338a4fd3ffa94968c85122437c81c0e4e7
Heiko Seeberger <git@heikoseeberger.de>
2025-11-24 11:00:07 +0100

    docs: fix README

--- examples/test.rs
+++ examples/test.rs
@@ -4,10 +4,10 @@ use axum::{Router, ServiceExt, response::IntoResponse, routing::get};
 use tokio::net::TcpListener;
 use tower::Layer;
 
+const API_VERSIONS: ApiVersions<2> = ApiVersions::new([0, 1]);
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    const API_VERSIONS: ApiVersions<2> = ApiVersions::new([0, 1]);
-
     let app = Router::new()
         .route("/ready", get(ready))
         .route("/api/v0/test", get(ok_0))
