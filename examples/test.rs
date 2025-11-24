c19552e9c139ae042013cdf62a76a8d49d9a7c88
Heiko Seeberger <git@heikoseeberger.de>
2025-11-24 10:23:08 +0100

    docs: extend/improve Rustdoc

--- examples/test.rs
+++ examples/test.rs
@@ -4,15 +4,16 @@ use axum::{Router, ServiceExt, response::IntoResponse, routing::get};
 use tokio::net::TcpListener;
 use tower::Layer;
 
-const API_VERSIONS: ApiVersions<2> = ApiVersions::new([0, 1]);
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    const API_VERSIONS: ApiVersions<2> = ApiVersions::new([0, 1]);
+
     let app = Router::new()
         .route("/ready", get(ready))
         .route("/api/v0/test", get(ok_0))
         .route("/api/v1/test", get(ok_1));
-    let app = ApiVersionLayer::new("/", API_VERSIONS).layer(app);
+
+    let app = ApiVersionLayer::new("/api", API_VERSIONS).layer(app);
 
     let listener = TcpListener::bind(("0.0.0.0", 8080))
         .await
@@ -21,6 +22,7 @@ async fn main() -> anyhow::Result<()> {
         .await
         .context("run server")
 }
+
 async fn ready() -> impl IntoResponse {
     "ready"
 }
